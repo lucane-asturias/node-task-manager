8dc5ea1b0512ffb525705890eceac93c39362c5c
Lucane <lucassanntos01@gmail.com>
2024-11-18 05:25:07 -0800

    Express REST API & Resource Creation Endpoints

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+app.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
