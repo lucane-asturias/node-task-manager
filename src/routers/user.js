da0d7335ad5f0d0ded266fa1e66dc39b9719e2b7
Lucane <lucassanntos01@gmail.com>
2024-12-18 03:52:33 -0300

    Saving Avatar to User Profile & Auto-Cropping

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
@@ -93,7 +94,8 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.status(200).send()
 }, (error, req, res, next) => {
@@ -104,6 +106,19 @@ router.delete('/users/me/avatar', auth, upload.single('avatar'), async (req, res
     req.user.avatar = undefined
     await req.user.save()
     res.status(200).send()
+})
+
+router.get('users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) throw new Error()
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 module.exports = router
