0dd7966c4511253e3761d641ad8ca01ff0f4d60f
Lucane <lucassanntos01@gmail.com>
2024-12-23 01:12:28 -0300

    Testing Task Data Security

--- tests/task.test.js
+++ tests/task.test.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose')
 const app = require('../src/app')
 const Task = require('../src/models/task')
 
-const { userOneId, userOne, setupDatabase } = require('./utils/db')
+const { 
+    userOneId,
+    userOne,
+    userTwo,
+    userTwoId,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase
+} = require('./utils/db')
 
 beforeEach(setupDatabase)
 afterAll(async () => await mongoose.connection.close())
@@ -14,9 +23,27 @@ test('Should create task for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({ description: 'Test', completed: true })
         .expect(201)
-    
     const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.description).toBe('Test')
     expect(task.completed).toBe(true)
+})
+
+test('Should fetch user tasks', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+    expect(response.body).toHaveLength(2)
+})
+
+test('Should 404 when userTwo tries to delete other users tasks', async () => {
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
